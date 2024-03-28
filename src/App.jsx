d60f2355045ceeb2b7a50d580cec7c78db3ad5ad
Yashas V <130334248+yashas8197@users.noreply.github.com>
2024-03-28 12:15:11 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+export default function App() {
+  const [favorite, setFavorite] = useState({});
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const questions = [
+    {
+      id: 1,
+      question: "Favorite Color?",
+      options: ["Red", "Blue", "Green", "Yellow"],
+    },
+    {
+      id: 2,
+      question: "Favorite Animal?",
+      options: ["Dog", "Cat", "Bird", "Fish"],
+    },
+    {
+      id: 3,
+      question: "Favorite Food?",
+      options: ["Pizza", "Burger", "Pasta", "Salad"],
+    },
+  ];
+
+  const handleFavorite = (questionId, option) => {
+    setFavorite({ ...favorite, [questionId]: option });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (Object.keys(favorite).length >= 3) {
+      setFormSubmitted(true);
+    }
+  };
+
+  const Questions = () => {
+    if (favorite && !formSubmitted) {
+      return (
+        <form onSubmit={handleSubmit}>
+          {questions.map((question) => (
+            <div key={question.id}>
+              <h2>{question.question}</h2>
+              <ul>
+                {question.options.map((option) => (
+                  <li key={option}>
+                    <input
+                      type="radio"
+                      name={question.question.split(" ")[0] + question.id}
+                      value={option}
+                      onChange={() => handleFavorite(question.id, option)}
+                    />
+                    {option}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <button type="submit">Submit</button>
+        </form>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Thank you</h1>
+        </div>
+      );
+    }
+  };
+  return (
+    <main>
+      <h1>Polling App</h1>
+      <Questions />
+    </main>
+  );
+}
